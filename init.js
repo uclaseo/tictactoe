e78ba71c486a6d14a370a6d79dff99a000f11833
inseok <inseok.ucla@gmail.com>
2017-07-31 17:18:59 -0700

    play

--- init.js
+++ init.js
@@ -15,13 +15,6 @@ const viewBoard = () => {
 
 }
 
-viewBoard();
-
-prompt.get(['Pick a Number'], function(error, result) {
-  let number = result['Pick a Number'];
-  placeMove(number);
-  viewBoard();
-})
 
 const placeMove = (number) => {
   if (number === '1') {
@@ -32,7 +25,7 @@ const placeMove = (number) => {
     board[0][2] = '0'
   } else if (number === '4') {
     board[1][0] = '0'
-  } else if (number === 5) {
+  } else if (number === '5') {
     board[1][1] = '0'
   } else if (number === '6') {
     board[1][2] = '0'
@@ -43,4 +36,22 @@ const placeMove = (number) => {
   } else if (number === '9') {
     board[2][2] = '0'
   }
-}
+};
+
+const winCombinations = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], ['1', '5', '9'], ['3', '5', '7']];
+let isOver = false;
+viewBoard();
+
+const play = () => {
+  prompt.start();
+  prompt.get(['Pick a Number'], function(error, result) {
+    let number = result['Pick a Number'];
+    placeMove(number);
+    viewBoard();
+    if (!isOver) {
+      play();
+    }
+  });
+};
+
+play();
